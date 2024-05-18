89ba4e659b95b820e1a6c8967fdc182f5a38a704
Christopher Wolf <christopher-wolf@outlook.de>
2024-05-18 19:06:05 +0200

    refactor movement for smoother moves

--- game/game.py
+++ game/game.py
@@ -7,7 +7,6 @@ class Game:
     def __init__(self):
         # Initialize Pygame
         pygame.init()
-        
         # Set up the display
         self.screen = pygame.display.set_mode((settings.SCREEN_WIDTH, settings.SCREEN_HEIGHT))
         pygame.display.set_caption(settings.CAPTION)
@@ -16,7 +15,7 @@ class Game:
         self.lane_positions = settings.LANE_POSITIONS
         
         # Create a player instance
-        self.player = Player(self.lane_positions[1], 500, 50, 50, 100, self.lane_positions) # start in the middle lane
+        self.player = Player(self.lane_positions[1], self.lane_positions)
         
         # Clock to control frame rate
         self.clock = pygame.time.Clock()
@@ -25,6 +24,7 @@ class Game:
         self.running = True
     
     def handle_events(self):
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.running = False
@@ -35,28 +35,25 @@ class Game:
         if keys[pygame.K_RIGHT]:
             self.player.move_right()
     
-    def update(self):
-        # Here you can add more game logic updates if needed
-        pass
+    def update(self, dt):
+        self.player.update(dt)
     
     def draw(self):
-        # Fill the screen with a color (RGB: White)
+
         self.screen.fill((255, 255, 255))
         
         # Draw the player
         self.player.draw(self.screen)
         
-        # Update the display
         pygame.display.flip()
     
     def run(self):
         while self.running:
+            dt = self.clock.tick(settings.FRAME_RATE) / 1000.0 
+
             self.handle_events()
-            self.update()
+            self.update(dt)
             self.draw()
-            
-            # Cap the frame rate
-            self.clock.tick(settings.FRAME_RATE)
         
         self.quit()
     
