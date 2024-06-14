d2a8d845ea21bfd058d2d466fb71e19d66174855
Christopher Wolf <christopher-wolf@outlook.de>
2024-06-15 00:11:19 +0200

    test vvc gym enviornment

---
+++ gym_env/test.py
@@ -0,0 +1,27 @@
+import gymnasium as gym
+from env import VVEnv 
+
+def test_environment(env: VVEnv, episodes=5, mode='human', verbose=False):
+    for episode in range(episodes):
+        observation = env.reset()
+        is_done = False
+        total_reward = 0
+
+        while not is_done:
+            env.render()
+            action = env.action_space.sample()  # Randomly sample an action
+            observation, reward, done, info = env.step(action)
+            total_reward += reward
+            # Print observation and reward to see what's happening
+            if verbose:
+                print(f"Observation: {observation}")
+                print(f"Reward: {reward}, Total Reward: {total_reward}")
+
+            if done:
+                print(f"Episode {episode+1} finished with total reward: {total_reward}")
+                is_done = True
+        
+                
+if __name__ == "__main__":
+    env = VVEnv("human")
+    test_environment(env, episodes=150, mode="human")
