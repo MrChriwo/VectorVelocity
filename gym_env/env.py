2845707563d4e33115926fe094a2a39c7673c310
Christopher Wolf <christopher-wolf@outlook.de>
2024-06-24 14:49:43 +0200

    refactor env

--- gym_env/env.py
+++ gym_env/env.py
@@ -17,93 +17,146 @@ class VVEnv(gym.Env):
         self.mode = mode
         self.game = Game(self.mode)
         
-        # Define the action and observation space
         self.observation_space = spaces.Dict({
             "player_pos": spaces.Discrete(LANE_POSITIONS[-1]),
             "obstacles": spaces.Box(
-                low=np.zeros((9, 2), dtype=np.int32),
-                high=np.array([[SCREEN_HEIGHT, LANE_POSITIONS[-1]]]*9, dtype=np.int32),
+                low=np.full((9, 2), [-454, -1], dtype=np.int32),
+                high=np.full((9, 2), [SCREEN_HEIGHT + 120, LANE_POSITIONS[-1] + 90], dtype=np.int32),
                 dtype=np.int32),
             "coins": spaces.Box(
-                low=np.zeros((20, 2), dtype=np.int32),
-                high=np.array([[SCREEN_HEIGHT, LANE_POSITIONS[-1]]]*20, dtype=np.int32),
+                low=np.full((40, 2), [-710, -1], dtype=np.int32),
+                high=np.full((40, 2), [SCREEN_HEIGHT + 120, LANE_POSITIONS[-1] + 90], dtype=np.int32),
                 dtype=np.int32),
-            "score": spaces.Discrete(np.iinfo(np.int32).max),
-            "collected_coins": spaces.Discrete(np.iinfo(np.int32).max),
-            "speed": spaces.Box(low=4.0, high=MAXIMUM_SPEED, shape=(1,), dtype=np.float32)
-
+            "score": spaces.Discrete(10000),
+            "collected_coins": spaces.Discrete(20000),
+            "speed": spaces.Discrete(MAXIMUM_SPEED+1)
         })
-        self.action_space = spaces.Discrete(3)
+        self.action_space = spaces.Discrete(1)
         self.latest_speed = self.game.speed
+        self.current_reward = 0
+        self.dodged_obstacles = []
+        self.missed_coins = []
 
     def reset(self, seed=None):
         super().reset(seed=seed) 
         self.game.restart()
+        self.current_reward = 0
+        self.dodged_obstacles = []
+        self.missed_coins = []
+        self.latest_speed = self.game.speed
         return self._get_observation(), {}
 
     def step(self, action):
-
-        if action == 1:  # Move left
-            self.game.player.move_left()
-        elif action == 2:  # Move right
+        if action == 0: # Do nothing
+            self.game.player.x = self.game.player.get_current_positon()
+        elif action == 1:  # Move right
             self.game.player.move_right()
-        elif action == 0: # Do nothing
-            pass
+        elif action == 2: # move left
+            self.game.player.move_left()
+
 
         self.game.update(self.game.clock.tick(FRAME_RATE) / 1000.0)
         observation = self._get_observation()
         reward = self._get_reward()
         done = self.game.is_game_over()
+
         truncated = False
 
         return observation, reward, done, truncated, {}
 
     def _get_observation(self):
-        obstacles = np.array([[obstacle.y, obstacle.x] for obstacle in self.game.spawnMgr.obstacles])
-        coins = np.array([[coin.y, coin.x] for coin in self.game.spawnMgr.coins])
+        max_obstacles = 9
+        max_coins = 40
+        obstacles = np.full((max_obstacles, 2), -1, dtype=np.int32)
+        coins = np.full((max_coins, 2), -1, dtype=np.int32)
 
-        if len(obstacles) == 0:
-            obstacles = np.zeros((9, 2), dtype=np.int32)
-        if len(coins) == 0:
-            coins = np.zeros((20, 2), dtype=np.int32)
+        game_obstacles = np.array([[obstacle.y, obstacle.x] for obstacle in self.game.spawnMgr.obstacles])
+        game_coins = np.array([[coin.y, coin.x] for coin in self.game.spawnMgr.coins])
+        if game_obstacles.size > 0:
+            obstacles[:len(game_obstacles)] = game_obstacles
+        if game_coins.size > 0:
+            coins[:len(game_coins)] = game_coins
+
 
         observation = {
-            "player_pos": self.game.player.get_current_positon(),
+            "player_pos": int(self.game.player.get_current_positon()),
             "obstacles": obstacles,
             "coins": coins,
             "score": int(self.game.score),
             "collected_coins": self.game.collected_coins,
-            "speed": np.array([self.game.speed], dtype=np.float32)
+            "speed": int(self.game.speed)
         }
+
         return observation
+    
 
     def _get_reward(self):
-        reward = 0
+        reward = self.current_reward
         speed_factor = self.game.speed / 4 
         coin_reward = 10
-        score_reward = 20
+        # score_reward = 20
         game_over_penalty = 75
         dodged_obstacle_reward = 5
+        coin_missed_penalty = 4
 
         # Reward for dodging obstacles
         for obstacle in self.game.spawnMgr.obstacles:
-            if obstacle.y > self.game.player.y:
+            if obstacle.id in self.dodged_obstacles:
+                continue
+            if obstacle.y >= self.game.player.y +100 and not self.game.is_game_over():
+                self.dodged_obstacles.append(obstacle.id)
                 reward += dodged_obstacle_reward * speed_factor
+                # print("Dodged obstacle")
+            
+            for id in self.dodged_obstacles:
+                if id not in [obstacle.id for obstacle in self.game.spawnMgr.obstacles]:
+                    self.dodged_obstacles.remove(id)
+                    # print("Removed obstacle id", id)
+
 
         # Reward for collecting coins
         if self.game.collected_coins > self.game.last_updated_coins:
             reward += coin_reward * speed_factor
             self.game.last_updated_coins = self.game.collected_coins
+            # print("Collected coin")
 
-        # Reward for reaching score milestones
-        if int(self.game.score) % 500 == 0 and self.game.score != 0:
-            reward += score_reward * speed_factor
+        # missed coins penalty
+        for coin in self.game.spawnMgr.coins:
+            if coin.id in self.missed_coins:
+                continue
+            if coin.y >= self.game.player.y + 20:
+                self.missed_coins.append(coin.id)
+                reward -= coin_missed_penalty * speed_factor
+                # print("Missed coin")
+            
+            for id in self.missed_coins:
+                if id not in [coin.id for coin in self.game.spawnMgr.coins]:
+                    self.missed_coins.remove(id)
+                             
 
         # Penalty for game over
         if self.game.is_game_over():
             reward -= game_over_penalty * speed_factor
-        return reward
+
+        self.current_reward = reward
+        return int(reward)
     
     def render(self):
-        self.game.render(self.mode)
+        self.game.render()
 
+
+if __name__ == "__main__":
+    env = VVEnv()
+    env.reset()
+    for _ in range(10):
+        done = False
+        env.reset()
+        steps = 0
+        while not done:
+            env.render()
+            action = 0
+            observation, reward, done, truncated, info = env.step(2)
+            steps += 1
+            if done:
+                print(f"{20* '='}\nEPOCH {_ +1} DONE\nREWARD: {reward}\nSTEPS: {steps}\n{20* '='}")
+                steps = 0
