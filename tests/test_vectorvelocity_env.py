fa2cace55b7974c87778682a162f2c3a59e6ae26
Christopher Wolf <christopher-wolf@outlook.de>
2024-07-15 17:09:40 +0200

    extend observation space test

--- tests/test_vectorvelocity_env.py
+++ tests/test_vectorvelocity_env.py
@@ -1,6 +1,7 @@
 import unittest
-from gym_vectorvelocity.env import VectorVelocityEnv
 import numpy as np
+from gym_vectorvelocity.env import VectorVelocityEnv
+from gym_vectorvelocity.game.settings import MAXIMUM_SPEED
 
 class TestVectorVelocityEnv(unittest.TestCase):
     
@@ -21,28 +22,21 @@ class TestVectorVelocityEnv(unittest.TestCase):
         obs, info = self.env.reset()
         self.assertIsNotNone(obs)
         self.assertIsInstance(obs, dict)
-        self.assertIn("obstacles", obs)
-        self.assertIn("coins", obs)
-        self.assertIn("score", obs)
-        self.assertIn("player_pos", obs)
+        self.check_observation_space(obs)
 
     def test_step(self):
         """
         Test stepping through the environment
+        each step one time
         """
-        self.env.reset()
-        obs, reward, done, truncated, info = self.env.step(1)
-        self.assertIsInstance(obs, dict)
-        self.assertIn("obstacles", obs)
-        self.assertIn("coins", obs)
-        self.assertIn("score", obs)
-        self.assertIn("player_pos", obs)
-        self.assertIsInstance(reward, float)
-        self.assertIsInstance(done, bool)
-        self.assertIsInstance(truncated, bool)
-
-        # Check observation space
-        self.check_observation_space(obs)
+        for step in range(3):
+            self.env.reset()
+            obs, reward, done, truncated, info = self.env.step(step)
+            self.assertIsInstance(obs, dict)
+            self.assertIsInstance(reward, float)
+            self.assertIsInstance(done, bool)
+            self.assertIsInstance(truncated, bool)
+            self.check_observation_space(obs)
 
     def test_action_space(self):
         """
@@ -57,7 +51,13 @@ class TestVectorVelocityEnv(unittest.TestCase):
         obs_space = self.env.observation_space.spaces
         self.assertIn("obstacles", obs_space)
         self.assertIn("coins", obs_space)
+        self.assertIn("obstacle_dists", obs_space)
+        self.assertIn("coin_dists", obs_space)
+        self.assertIn("lane_obstacles", obs_space)
+        self.assertIn("lane_coins", obs_space)
         self.assertIn("score", obs_space)
+        self.assertIn("collected_coins", obs_space)
+        self.assertIn("speed", obs_space)
         self.assertIn("player_pos", obs_space)
 
     def check_observation_space(self, obs):
@@ -66,12 +66,54 @@ class TestVectorVelocityEnv(unittest.TestCase):
         """
         self.assertIn("obstacles", obs)
         self.assertIn("coins", obs)
+        self.assertIn("obstacle_dists", obs)
+        self.assertIn("coin_dists", obs)
+        self.assertIn("lane_obstacles", obs)
+        self.assertIn("lane_coins", obs)
         self.assertIn("score", obs)
+        self.assertIn("collected_coins", obs)
+        self.assertIn("speed", obs)
         self.assertIn("player_pos", obs)
+
         self.assertIsInstance(obs["obstacles"], np.ndarray)
+        self.assertEqual(obs["obstacles"].shape, (self.env.NUM_MAX_OBSTACLES * 2,))
+        self.assertTrue((obs["obstacles"] >= -1).all() and (obs["obstacles"] <= 1).all())
+
         self.assertIsInstance(obs["coins"], np.ndarray)
+        self.assertEqual(obs["coins"].shape, (self.env.NUM_MAX_COINS * 2,))
+        self.assertTrue((obs["coins"] >= -1).all() and (obs["coins"] <= 1).all())
+
+        self.assertIsInstance(obs["obstacle_dists"], np.ndarray)
+        self.assertEqual(obs["obstacle_dists"].shape, (self.env.NUM_MAX_OBSTACLES * 2,))
+        self.assertTrue((obs["obstacle_dists"] >= -1).all() and (obs["obstacle_dists"] <= 1).all())
+
+        self.assertIsInstance(obs["coin_dists"], np.ndarray)
+        self.assertEqual(obs["coin_dists"].shape, (self.env.NUM_MAX_COINS * 2,))
+        self.assertTrue((obs["coin_dists"] >= -1).all() and (obs["coin_dists"] <= 1).all())
+
+        self.assertIsInstance(obs["lane_obstacles"], np.ndarray)
+        self.assertEqual(obs["lane_obstacles"].shape, (self.env.NUM_MAX_OBSTACLES,))
+        self.assertTrue((obs["lane_obstacles"] >= 0).all() and (obs["lane_obstacles"] <= self.env.NUM_LANES).all())
+
+        self.assertIsInstance(obs["lane_coins"], np.ndarray)
+        self.assertEqual(obs["lane_coins"].shape, (self.env.NUM_MAX_COINS,))
+        self.assertTrue((obs["lane_coins"] >= 0).all() and (obs["lane_coins"] <= self.env.NUM_LANES).all())
+
         self.assertIsInstance(obs["score"], int)
+        self.assertGreaterEqual(obs["score"], 0)
+        self.assertLessEqual(obs["score"], 120000)
+
+        self.assertIsInstance(obs["collected_coins"], int)
+        self.assertGreaterEqual(obs["collected_coins"], 0)
+        self.assertLessEqual(obs["collected_coins"], 20000)
+
+        self.assertIsInstance(obs["speed"], int)
+        self.assertGreaterEqual(obs["speed"], 0)
+        self.assertLessEqual(obs["speed"], MAXIMUM_SPEED)
+
         self.assertIsInstance(obs["player_pos"], np.ndarray)
+        self.assertEqual(obs["player_pos"].shape, (1,))
+        self.assertTrue((obs["player_pos"] >= 0).all() and (obs["player_pos"] <= 1).all())
 
     def test_random_moves(self):
         """
@@ -88,7 +130,6 @@ class TestVectorVelocityEnv(unittest.TestCase):
                 self.assertIsInstance(reward, float)
                 self.assertIsInstance(done, bool)
                 self.assertIsInstance(truncated, bool)
-
                 self.check_observation_space(obs)
 
 if __name__ == '__main__':
