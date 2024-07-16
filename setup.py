47d1e166689a3ef5d7ba29d5f3fb275a78de27f2
Christopher Wolf <christopher-wolf@outlook.de>
2024-07-16 18:38:54 +0200

    release hotfix

--- setup.py
+++ setup.py
@@ -2,7 +2,17 @@ from setuptools import setup, find_packages
 import os
 
 CI_VERSION = os.getenv('CI_VERSION')
-CI_LICENCE = os.getenv('CI_LICENCE')
+CI_LICENSE = os.getenv('CI_LICENSE')
+
+if not CI_VERSION:
+    raise ValueError("CI_VERSION environment variable is not set")
+if not CI_LICENSE:
+    raise ValueError("CI_LICENSE environment variable is not set")
+
+long_description = ''
+if os.path.exists('README.md'):
+    with open('README.md', 'r', encoding='utf-8') as fh:
+        long_description = fh.read()
 
 setup(
     name='vector-velocity-gym',
@@ -11,10 +21,10 @@ setup(
     maintainer="MrChriwo",
     url="https://github.com/MrChriwo/VectorVelocity",
     author='MrChriwo & Stevenschneider',
-    long_description=open('README.md').read(),
+    long_description=long_description,
     long_description_content_type='text/markdown',
     packages=find_packages(exclude=('tests', 'tests.*')),
-    license=CI_LICENCE,
+    license=CI_LICENSE,
     install_requires=[
         'gymnasium',
         'pygame',
